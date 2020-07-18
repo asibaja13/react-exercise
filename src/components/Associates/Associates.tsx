95d3dab7bc6a80a24a5269d38ab05d98edfa77a6
Allan Sibaja <allan.sibaja@contractors.accruent.com>
2020-07-18 01:49:26 -0600

    Get associates from db

--- src/components/Associates/Associates.tsx
+++ src/components/Associates/Associates.tsx
@@ -1,26 +1,38 @@
 import React, { Component } from "react";
 import Paper from "@material-ui/core/Paper";
 import AssociatesTable from "./Table";
+import { getAssociates } from '../../services/associate.service';
+import Associate from "../../interfaces/associate";
 
-export default class Associates extends Component {
-  getAssociates = () => {
-    const associates = [];
-    for (let i = 0; i < 20; i++) {
-      associates.push({
-        id: i,
-        first_name: "Adaline",
-        last_name: "Zanni",
-        email: "azanni0@kickstarter.com",
-        Department: "Training"
-      });
-    }
-    return associates;
-  };
+interface Props {
+
+}
+interface State {
+  associates: Array<Associate>
+}
+
+export default class Associates extends Component<Props, State> {
+  
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      associates: []
+    };
+  }
+
+  async componentWillMount() {
+    const associates = await getAssociates();
+    this.setState({
+      associates
+    })
+    
+  }
 
   render() {
+    const { associates } = this.state;
     return (
       <Paper>
-        <AssociatesTable associates={this.getAssociates()} />;
+        <AssociatesTable associates={associates} />;
       </Paper>
     );
   }
---
+++ src/services/associate.service.ts
@@ -0,0 +1,7 @@
+import Associate from "../interfaces/associate";
+
+export async function getAssociates() {
+    const response = await fetch('http://localhost:3001/associates');
+    const body = await response.json();
+    return body as Array<Associate>; 
+}
